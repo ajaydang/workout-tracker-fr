8b12a4dd6e467c35038f629f12f3d92f51f34a13
ajaydangol014 <ajaydangol014@gmail.com>
2024-12-12 15:11:40 +0545

    Add login functionality and styling for workout tracker

---
+++ src/pages/Login.js
@@ -0,0 +1,103 @@
+import { Card, Flex, Typography } from 'antd';
+import { Button, Checkbox, Form, Input } from 'antd';
+import React from 'react';
+import { loginUser } from '../services/user';
+
+const Login = () => {
+	const { Title, Paragraph, Text, Link } = Typography;
+
+	const onFinish = async (values) => {
+		const payload = {
+			email: values.email,
+			password: values.password,
+		};
+		const { data } = await loginUser(payload);
+
+		if (data.data.token) {
+			localStorage.setItem('workout-token', data.data.token);
+			window.location.href = 'http://localhost:3000/dashboard';
+		} else {
+			throw Error('Form Error.');
+		}
+	};
+
+	const onFinishFailed = (errorInfo) => {
+		console.log('Failed:', errorInfo);
+	};
+	const validateMessages = {
+		required: '${label} is required!',
+		types: {
+			email: '${label} is not a valid email!',
+			number: '${label} is not a valid number!',
+		},
+	};
+	return (
+		<Flex>
+			<Card title='Login'>
+				<Form
+					name='basic'
+					labelCol={{
+						span: 8,
+					}}
+					wrapperCol={{
+						span: 16,
+					}}
+					style={{
+						maxWidth: 600,
+					}}
+					initialValues={{
+						remember: true,
+					}}
+					onFinish={onFinish}
+					onFinishFailed={onFinishFailed}
+					autoComplete='off'
+					validateMessages={validateMessages}
+				>
+					<Form.Item
+						label='Email'
+						name='email'
+						rules={[
+							{
+								required: true,
+								type: 'email',
+							},
+						]}
+					>
+						<Input />
+					</Form.Item>
+
+					<Form.Item
+						label='Password'
+						name='password'
+						rules={[
+							{
+								required: true,
+							},
+						]}
+					>
+						<Input.Password />
+					</Form.Item>
+
+					{/* <Form.Item
+						name='remember'
+						valuePropName='checked'
+						label={null}
+					>
+						<Checkbox>Remember me</Checkbox>
+					</Form.Item> */}
+
+					<Form.Item label={null}>
+						<Button
+							type='primary'
+							htmlType='submit'
+						>
+							Submit
+						</Button>
+					</Form.Item>
+				</Form>
+			</Card>
+		</Flex>
+	);
+};
+
+export default Login;
